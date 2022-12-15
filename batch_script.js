bd3918de9a6c58dca65c094d2667a845c422b95c
amiraabouhadid <amiraabouhadid@gmail.com>
2022-12-15 13:55:42 +0200

    add callback

--- batch_script.js
+++ batch_script.js
@@ -1,30 +1,51 @@
-const { Client } = require('./lib/client')
+const { Client } = require('./lib/client');
 
+async function syncBatch() {
+	try {
+		const cl = new Client(
+			'wss://tfchain.dev.grid.tf/ws',
+			'priority remain raw spice degree head retire fan syrup modify helmet uniform',
+			'sr25519',
+		);
 
-async function syncBatch(){
+		await cl.init();
+		console.log('cl.api.tx.utility', cl.api.tx.utility.batch);
+		console.log('client initialized');
+		const names = ['testvm2', 'testvm3', 'testvm4'];
+		const extrinsics = [];
+		console.log('typeof extrensics is', extrinsics);
+		for (const name of names) {
+			const createdContract = await cl
+				.createNameContract(name, (res) => {
+					if (res instanceof Error) {
+						console.log(res);
+					}
+					const { events, status } = res;
 
-  try {
-    const cl = new Client("wss://tfchain.dev.grid.tf/ws", "priority remain raw spice degree head retire fan syrup modify helmet uniform", "sr25519");
+					switch (status.type) {
+						case 'Ready':
+							console.log('done');
+					}
+					if (status.isFinalized) {
+						console.log('finalized');
+					} else {
+						events.forEach(({ phase, event: { data, method, section } }) => {
+							console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
+						});
+					}
+				})
+				.catch((err) => console.log(err.message));
+			console.log('createdContract', createdContract);
+			extrinsics.push(createdContract);
+			console.log(`new contract with name ${name} pushed`);
+		}
 
-    await cl.init();
-    console.log("cl.api.tx.utility", cl.api.tx.utility.batch);
-    console.log("client initialized");
-    const names = ["testvm2", "testvm3", "testvm4"];
-    const extrinsics = [];
-    console.log("typeof extrensics is", extrinsics);
-    for(const name of names){
-      const createdContract = await cl.createNameContract(name);
-      console.log("createdContract", createdContract);
-      extrinsics.push(createdContract);
-      console.log(`new contract with name ${name} pushed`);
-    }
-  
-    const batchCreate = await cl.api.tx.utility.batch(extrinsics);
-    console.log("batchCreate", batchCreate);
-    console.log(`extrinsics are ${extrinsics}`, typeof extrinsics);
-  } catch (error) {
-    console.log(error);
-  }
+		const batchCreate = await cl.api.tx.utility.batch(extrinsics);
+		console.log('batchCreate', batchCreate);
+		console.log(`extrinsics are ${extrinsics}`, typeof extrinsics);
+	} catch (error) {
+		console.log(error);
+	}
 }
-  
+
 syncBatch();
